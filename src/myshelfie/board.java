10ff6f759e93ac0496632e1e32e6818af778c1cc
mrosolia <m.rosolia@studenti.unibg.it>
2023-03-28 14:52:51 +0200

    secondo aggiornamento di board

--- src/myshelfie/board.java
+++ src/myshelfie/board.java
@@ -5,5 +5,41 @@ public class board {
 
 	matrix game= new matrix();
 	game.matrix(9,9);
+	game[1][1]=7;
+	game[1][2]=7;
+	game[1][3]=7;
+	game[1][6]=7;
+	game[1][7]=7;
+	game[1][8]=7;
+	game[1][9]=7;
+	game[2][1]=7;
+	game[2][2]=7;
+	game[2][3]=7;
+	game[2][7]=7;
+	game[2][8]=7;	
+	game[2][9]=7;
+	game[3][2]=7;
+	game[3][8]=7;
+	game[3][9]=7;
+	game[4][1]=7;
+	game[7][1]=7;
+	game[7][2]=7;
+	game[7][8]=7;
+	game[7][9]=7;
+	game[8][1]=7;
+	game[8][2]=7;
+	game[8][3]=7;
+	game[8][7]=7;
+	game[8][8]=7;
+	game[8][9]=7;
+	game[9][1]=7;
+	game[9][2]=7;
+	game[9][3]=7;
+	game[9][4]=7;
+	game[9][7]=7;
+	game[9][8]=7;
+	game[9][9]=7;
+	if (playercount==)
+	
 	
 }
