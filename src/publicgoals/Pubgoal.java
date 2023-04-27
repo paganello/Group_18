4d5705e75abe01de148595b52881be943d01fb0b
Thomas Paganelli <thomas.paganelli03@gmail.com>
2023-04-27 13:05:43 +0200

    *da completare*

--- src/publicgoals/Pubgoal.java
+++ src/publicgoals/Pubgoal.java
@@ -2,4 +2,10 @@ package publicgoals;
 
 public class Pubgoal {
 	//gli obbiettivi da verificare in ogni turnCheck=22;
+	boolean done = false;
+	int goal1 = 0, goal2 = 0; //possono andare da 1 a 12
+	
+	int SetGoals(int i, int j) {
+		
+	}
 }
