909fd66678012271f7dbbc37376f7257acb82287
Thomas paganelli <thomas.paganelli03@gmail.com>
2023-05-05 18:44:23 +0200

    Aggiornati modificatori di accesso e aggiornato costruttore.
    tutti attributi messi a private e creati rispettivi getter and setter
    (setter aggiunto solo agli attributi che possono variare).

--- src/myshelfie/Player.java
+++ src/myshelfie/Player.java
@@ -4,33 +4,77 @@ import java.util.Scanner;
 public class Player {
 	
 	//dichiarazione variabile classe player
-	String nome;
-	int points;
-	boolean chair;
-	int numShelf;
-	int privateGoal;
-	int[] publicGoals;
+	private String name;
+	private int points = 0;
+	private boolean chair;
+	private Privgoal privateGoal;
+	private Pubgoal publicGoals;
 	BoxMatrix shelf; 
 	
 	//costruttore per il player
-	public Player (int order) {
+	public Player (int nPlayer, int pGoal_1, int pGoal_2) {
 		
-		points = 0;
-		//da aggiungere asseganzione dei goals
-		this.chair = false;
-		
-		//assegnazione sedia al primo player
-		if(order == 1) {
-			this.chair = true;
-		}
-		//richiesta nome
-		System.out.println("Inserisci il nome del giocatore " + (order+1) + ": ");
+		System.out.println("Inserisci il nome del giocatore " + (nPlayer+1) + ": ");	//set name
 		Scanner sc = new Scanner(System.in);
-		this.nome = sc.nextLine();
+		this.name = sc.nextLine();
 		sc.close();
 		
-		//creazione libreria del player 
-		this.numShelf = order;
-		this.shelf = new BoxMatrix(6,5);
+		if(nPlayer == 1) {	//set chair
+			this.chair = true;
+		}else {
+			this.chair = false;
+		}
+		
+		this.shelf = new BoxMatrix(6,5);	//creazione libreria del player 
+		
+		this.privateGoal = new Privgoal();	//assegan goal personale
+	}
+	
+	
+	public String getName() {
+	
+		return name;
+	}
+	
+
+	public int getPoints() {
+		
+		return points;
+	}
+	
+
+	public void setPoints(int points) {
+		
+		this.points = points;
+	}
+	
+
+	public boolean isChair() {
+		
+		return chair;
+	}
+	
+	
+	public Privgoal getPrivateGoal() {
+		
+		return privateGoal;
+	}
+	
+
+	public void setPrivateGoal(Privgoal privateGoal) {
+		
+		this.privateGoal = privateGoal;
+	}
+
+	
+	public Pubgoal getPublicGoals() {
+		
+		return publicGoals;
+	}
+	
+
+	public void setPublicGoals(Pubgoal publicGoal) {
+		
+		this.publicGoals = publicGoal; 
 	}
 }
