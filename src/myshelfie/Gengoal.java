c1f8ca06589652198964eb65baf081845b7076c5
Thomas Paganelli <thomas.paganelli03@gmail.com>
2023-05-11 11:19:14 +0200

    Lovoro Gruppo 11/05/2023: inizializzata classe Gengoal
    da modificare player per aggiungere apposito attributo

--- src/myshelfie/Gengoal.java
+++ src/myshelfie/Gengoal.java
@@ -2,4 +2,24 @@ package myshelfie;
 
 public class Gengoal {
 	//gli obbiettivi sempre validi da verificare in fase pointCheck
+	private boolean done;
+	private int points;
+	
+	public Gengoal(){
+		this.done = false;
+		this.points = 0;
+	}
+	
+	public boolean isDone(){
+		return this.done;
+	}
+	
+	public int getPoints(){
+		return this.points	
+	}
+	
+	public void computePoints(){
+		//funzione che computa il numero di punti dei goal generici sempre validi, ritorna il valore in points	
+	}
+	
 }
