824281b4a1e72a78e03d9f92c31e184b393e9c62
mrosolia <m.rosolia@studenti.unibg.it>
2023-05-17 13:04:12 +0200

    Introduzione della generazione dei goal pubblici, privati e player in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,8 @@
 package main;
-
+import java.util.*;
+import java.lang.*;
+import goals.*;
+import main.Player;
 public class Main {	
 	public static void main(String[] args) {
 		
@@ -23,10 +26,33 @@ public class Main {
 	*/
 	GameState stato;
 	stato = GameState.drawPhase;
+	int playerNum;
+	ArrayList<Player> listaPlayer= new ArrayList<Player>();
+	
+	Scanner sc= new Scanner(System.in);
 	do {
 		
 		switch (stato) {
 			case drawPhase:
+				Pubgoal obbPubblici=new Pubgoal(); //genero goal pubblici
+				
+				System.out.println ("Inserire il numero di giocatori"); //ottengo in input il numero di giocatori, che è
+				do {													//verificato alla fine di questo looè
+				playerNum= sc.nextInt();
+				if(playerNum<2 || playerNum>4) {
+					System.out.println("Per favore inserire solo valori tra 2 e 4");
+				}
+				else {
+					for(int contaPlayerLoop=0; contaPlayerLoop<playerNum; contaPlayerLoop++) {
+						Privgoal obb1= new Privgoal(); //genero due goal privati 
+						Privgoal obb2= new Privgoal();
+						listaPlayer.add(new Player((contaPlayerLoop+1),obb1.getGoalNumber(),obb2.getGoalNumber()));
+						}
+					
+				}
+				
+				}
+				while(playerNum<2 && playerNum>4);
 			
 				break;
 		
